2875ce801120a0f6685c695f89668b5ee5eb16e0
Pedro <pedroinacio061@hotmail.com>
2024-05-20 23:46:28 -0300

    modifica slider

--- SCRIPT/home.js
+++ SCRIPT/home.js
@@ -1,30 +1,30 @@
-let time = 3000,
-    currentImageIndex = 0,
-    images = document
-                .querySelectorAll("#slider img")
-    max = images.length;
+let slideIndex = 0;
+const slides = document.querySelectorAll(".input");
 
-function nextImage() {
-    
-    images[currentImageIndex]
-    .classList.remove("selected")
-
-    currentImageIndex++;
-    
-    if(currentImageIndex >=max)
-        currentImageIndex = 0;
-
-    console.log(currentImageIndex);
-    
-    images[currentImageIndex]
-    .classList.add("selected")
+function showSlide(n) {
+    slides.forEach((slide, index) => {
+        slide.checked = (index === n);
+    });
 }
 
-function start () {
-    setInterval(() => {
-        nextImage();
-    }, time)
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    showSlide(slideIndex);
 }
 
-window.addEventListener("load", start)
+function previousSlide() {
+    slideIndex--;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    showSlide(slideIndex);
+}
 
+document.querySelector(".navigation").addEventListener("click", function(event) {
+    if (event.target.classList.contains("bar")) {
+        let index = Array.from(event.target.parentNode.children).indexOf(event.target);
+        slideIndex = index;
+        showSlide(slideIndex);
+    }
+});
+
+setInterval(nextSlide, 6000);
