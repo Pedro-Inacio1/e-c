66df78f38ed89e275bfe08d0833afef562174914
Pedro-Inacio1 <pedroinacio061@hotmail.com>
2024-07-31 10:54:34 -0300

    atualiza front-ent da home e adiciona responsividade na pagina

--- App/SCRIPT/home.js
+++ App/SCRIPT/home.js
@@ -3,13 +3,13 @@ let slideIndex = 0;
 let images = document.querySelectorAll(".slider img");
 let max = images.length;
 
-function nextImage () {
-    
+function nextImage() {
+
     images[slideIndex].classList.remove("selected")
-    
+
     slideIndex++
 
-    if(slideIndex >= max) {
+    if (slideIndex >= max) {
         slideIndex = 0
     }
 
@@ -20,7 +20,19 @@ function start() {
     setInterval(() => {
 
         nextImage()
-    } , time)
+    }, time)
 }
 
 window.addEventListener('load', start)
+
+var homeIcon = document.getElementById("menu");
+
+homeIcon.addEventListener('click', () => {
+    let submenu = document.getElementById("test");
+    if (submenu.style.display === 'flex') {
+        submenu.style.display = 'none'
+    }
+    else {
+        submenu.style.display = 'flex'
+    }
+})
